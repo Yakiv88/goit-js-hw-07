505f834b0b6af0ede3de1e9cf9814e3a6e9b03b7
Yakiv88 <mickki33@gmail.com>
2021-07-19 15:02:45 +0300

    по3

---
+++ js/task-03.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+
+const galleryList = document.querySelector('#gallery');
+const addGalleryListEl = images.map(element => {
+  const addGalleryListElImg = `<li class="gallery__item"><a href="https://goit.global/javascript/ru/v1/module-07/props-attrs.html" class="link3"><img src="${element.url}" alt="${element.alt}"
+   width=320px height=150></a></li>`;
+  return addGalleryListElImg;
+});
+galleryList.insertAdjacentHTML("beforeend", addGalleryListEl.join(' '));
+galleryList.setAttribute("style", "list-style:none; display: flex; justify-content: space-around;")
